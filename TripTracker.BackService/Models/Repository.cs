ca1f6a7c027fd2b8c58ad28eb4331284449e1e12
Bringfireh <ngutortsenongo@gmail.com>
2022-07-16 23:27:04 +0100

    modified repository class

--- TripTracker.BackService/Models/Repository.cs
+++ TripTracker.BackService/Models/Repository.cs
@@ -7,5 +7,51 @@ namespace TripTracker.BackService.Models
 {
     public class Repository
     {
+        private List<Trip> trips = new List<Trip>() {
+            new Trip
+            {
+                Id = 1,
+                Name = "MVP Summit",
+                StartDate = new DateTime(2018,3,5),
+                EndDate = new DateTime(2018,3,8)
+            },
+            new Trip
+            {
+                Id = 2,
+                Name = "DevIntersection Orlando 2018",
+                StartDate = new DateTime(2018,3,25),
+                EndDate = new DateTime(2018,3,28)
+            },
+            new Trip
+            {
+                Id = 3,
+                Name = "Build 2018",
+                StartDate = new DateTime(2018,5,7),
+                EndDate = new DateTime(2018,5,9)
+            }
+        };
+        public List<Trip> Get()
+        {
+            return trips;
+        }
+        public Trip Get(int id)
+        {
+            return trips.First(t => t.Id == id);
+        }
+        public void Add(Trip mytrip)
+        {
+            trips.Add(mytrip);
+        }
+
+        public void Update(Trip ttrip)
+        {
+            trips.Remove(trips.First(t => t.Id == ttrip.Id));
+            Add(ttrip);
+        }
+        public void Remove(int id)
+        {
+            trips.Remove(trips.First(t => t.Id == id));
+        }
+
     }
 }
