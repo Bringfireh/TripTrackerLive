2de157051a21e8a633c22631c4250b83f974ad33
Bringfireh <ngutortsenongo@gmail.com>
2022-07-16 23:32:29 +0100

    added TripsController

---
+++ TripTracker.BackService/Controllers/TripsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TripTracker.BackService.Controllers
+{
+    public class TripsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- TripTracker.BackService/Models/Trip.cs
+++ TripTracker.BackService/Models/Trip.cs
@@ -7,6 +7,10 @@ namespace TripTracker.BackService.Models
 {
     public class Trip
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
 
     }
 }
