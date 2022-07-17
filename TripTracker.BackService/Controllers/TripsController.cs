225b51c68fc48023d6d86ba0dffc5d4750e5cbe9
Bringfireh <ngutortsenongo@gmail.com>
2022-07-18 00:09:39 +0100

    completed the API Endpoints

--- TripTracker.BackService/Controllers/TripsController.cs
+++ TripTracker.BackService/Controllers/TripsController.cs
@@ -3,41 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TripTracker.BackService.Models;
 
 namespace TripTracker.BackService.Controllers
 {
     [Route("api/[controller]")]
     public class TripsController : Controller
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public TripsController(Repository repository)
         {
-            return new string[] { "value1", "value2" };
+            _repository = repository;
+        }
+        private Repository _repository;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Trip>> Get()
+        {
+            return _repository.Get();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Trip> Get(int id)
         {
-            return "value";
+            return _repository.Get(id);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Trip value)
         {
+            _repository.Add(value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Trip value)
         {
+            _repository.Update(value);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            _repository.Remove(id);
         }
     }
 }
