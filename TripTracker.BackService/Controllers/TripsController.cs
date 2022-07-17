7f3b945c3090d5637ecd3cf427faae26a5246d98
Bringfireh <ngutortsenongo@gmail.com>
2022-07-17 21:24:51 +0100

    modified TripsController

--- TripTracker.BackService/Controllers/TripsController.cs
+++ TripTracker.BackService/Controllers/TripsController.cs
@@ -6,11 +6,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TripTracker.BackService.Controllers
 {
+    [Route("api/[controller]")]
     public class TripsController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
         {
-            return View();
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
         }
     }
 }
