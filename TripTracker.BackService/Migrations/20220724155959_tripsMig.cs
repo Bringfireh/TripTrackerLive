8f741088d006f66a11e3988f1122020f251a8ec1
Bringfireh <ngutortsenongo@gmail.com>
2022-07-24 17:27:35 +0100

    added migrations and updated db based on changes

---
+++ TripTracker.BackService/Migrations/20220724155959_tripsMig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TripTracker.BackService.Migrations
+{
+    public partial class tripsMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "trips",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_trips", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "trips");
+        }
+    }
+}
--- TripTracker.BackService/Startup.cs
+++ TripTracker.BackService/Startup.cs
@@ -31,7 +31,7 @@ namespace TripTracker.BackService
            // services.AddTransient<Models.Repository>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            string connectionString = @"data source=.\SqlExpress;initial catalog=TripTracking;integrated security=True;MultipleActiveResultSets=True;App=EntityFrameworkCore";
+            string connectionString = @"data source=.\SqlExpress;initial catalog=TripTracking;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
             
             //services.AddDbContext<TripContext>(o => o.UseSqlite("Data Source =JeffTrips.db"));
 
