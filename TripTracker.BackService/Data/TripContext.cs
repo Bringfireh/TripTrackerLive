553af81a844ea56f68ce415f5b9deefaa2b0ee8f
Bringfireh <ngutortsenongo@gmail.com>
2022-07-24 17:34:15 +0100

    uncommented the seeding code

--- TripTracker.BackService/Data/TripContext.cs
+++ TripTracker.BackService/Data/TripContext.cs
@@ -24,43 +24,43 @@ namespace TripTracker.BackService.Data
         //}
         public static void SeedData(IServiceProvider serviceProvider)
         {
-            //using (
-            //        var serviceScope = serviceProvider
-            //        .GetRequiredService<IServiceScopeFactory>().CreateScope())
-            //{
-            //    var context = serviceScope
-            //              .ServiceProvider.GetService<TripContext>();
+            using (
+                    var serviceScope = serviceProvider
+                    .GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope
+                          .ServiceProvider.GetService<TripContext>();
 
-            //    context.Database.EnsureCreated();
+                context.Database.EnsureCreated();
 
-            //    if (context.trips.Any()) return;
-            //    context.trips.AddRange(new Trip[]
-            //    {
+                if (context.trips.Any()) return;
+                context.trips.AddRange(new Trip[]
+                {
 
-            //    new Trip
-            //    {
-            //        Id = 1,
-            //        Name = "MVP Summit",
-            //        StartDate = new DateTime(2018, 3, 5),
-            //        EndDate = new DateTime(2018, 3, 8)
-            //    },
-            //new Trip
-            //{
-            //    Id = 2,
-            //    Name = "DevIntersection Orlando 2018",
-            //    StartDate = new DateTime(2018, 3, 25),
-            //    EndDate = new DateTime(2018, 3, 28)
-            //},
-            //new Trip
-            //{
-            //    Id = 3,
-            //    Name = "Build 2018",
-            //    StartDate = new DateTime(2018, 5, 7),
-            //    EndDate = new DateTime(2018, 5, 9)
-            //}
-            //    });
-            //    context.SaveChanges();
-            //}
+                new Trip
+                {
+                    Id = 1,
+                    Name = "MVP Summit",
+                    StartDate = new DateTime(2018, 3, 5),
+                    EndDate = new DateTime(2018, 3, 8)
+                },
+            new Trip
+            {
+                Id = 2,
+                Name = "DevIntersection Orlando 2018",
+                StartDate = new DateTime(2018, 3, 25),
+                EndDate = new DateTime(2018, 3, 28)
+            },
+            new Trip
+            {
+                Id = 3,
+                Name = "Build 2018",
+                StartDate = new DateTime(2018, 5, 7),
+                EndDate = new DateTime(2018, 5, 9)
+            }
+                });
+                context.SaveChanges();
+            }
         }
     }
 }
